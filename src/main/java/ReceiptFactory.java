87a774a097562de8757db8b1b071df9b31a0bbd0
alsu <alsu.shakirzyanova@3rdplace.com>
2017-04-18 22:20:29 +0200

    receipt con quantità diverse

--- src/main/java/ReceiptFactory.java
+++ src/main/java/ReceiptFactory.java
@@ -1,5 +1,7 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class ReceiptFactory {
 
@@ -12,15 +14,37 @@ public class ReceiptFactory {
     }
 
     public Receipt create(ArrayList<Product> products) {
-        ArrayList<ReceiptProduct> receiptProducts = new ArrayList<ReceiptProduct>();
-        for (Product product: products) {
-            ReceiptProduct receiptProduct = new ReceiptProduct(product, 1, priceCalculator.calculate(product));
-            receiptProducts.add(receiptProduct);
-        }
+        ArrayList<ReceiptProduct> receiptProducts = createReceiptProductList(products);
         BigDecimal totalPrice = priceCalculator.calculate(products);
         BigDecimal totalTax = taxCalculator.calculate(products);
 
         Receipt receipt = new Receipt(receiptProducts, totalPrice, totalTax);
         return receipt;
+    }
+
+    private ArrayList<ReceiptProduct> createReceiptProductList(ArrayList<Product> products) {
+        HashMap<String, Integer> productToQuantityMap = new HashMap<String, Integer>();
+        for (Product product : products) {
+            String productId = product.getProductId();
+            if ((productToQuantityMap.get(productId) == null)) {
+                productToQuantityMap.put(productId, 1);
+            } else {
+                productToQuantityMap.put(productId, (productToQuantityMap.get(productId) + 1));
+            }
+        }
+
+        HashSet<String> productAlreadyAdded = new HashSet<String>();
+        ArrayList<ReceiptProduct> receiptProducts = new ArrayList<ReceiptProduct>();
+        for (Product product : products) {
+            if (!productAlreadyAdded.contains(product.getProductId())) {
+                int productQuantity = productToQuantityMap.get(product.getProductId());
+                ReceiptProduct receiptProduct = new ReceiptProduct(product, productQuantity, priceCalculator.calculate(product, productQuantity));
+                receiptProducts.add(receiptProduct);
+                productAlreadyAdded.add(product.getProductId());
+            }
+
+        }
+
+        return receiptProducts;
     }
 }
